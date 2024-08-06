29f3b60814676800dc6c7f3ec2d2765aef624181
EmenceLastraops <EMencel@nycstudents.net>
2024-08-06 16:04:42 +0000

    Bulit an age verification

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+// console.log('hello world')
+
+// var prompt= prompt("What is your name?") 
+
+// console.log(prompt)
+
+
+function verifyAge (age){
+  var age = prompt("what is your age?")
+  console.log(age)
+
+  
+  if (age < 18) { alert ("you are a minor!")  
+  } else if (age >= 18 && age < 60) {
+    alert("You are an adult!")
+  }else{
+    alert("you are a senior citizen!")
+  }
+
+}
